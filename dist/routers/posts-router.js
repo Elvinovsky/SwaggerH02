9b1561426ce9ad8704508d856d216b46599d5965
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-03-09 17:53:29 +0500

    correct authGuardMiddleware

--- dist/routers/posts-router.js
+++ dist/routers/posts-router.js
@@ -4,14 +4,23 @@ exports.postsRouter = void 0;
 const express_1 = require("express");
 const posts_repository_1 = require("../repositories/posts-repository");
 const check_errors_1 = require("../errors/check-errors");
-const authGuardMiddleware = (req, res, next) => {
-    if (req.query.login === "admin" && req.query.password === "qwerty") {
-        next();
+const authGuardMiddleware = ((req, res, next) => {
+    // -----------------------------------------------------------------------
+    // authentication middleware
+    const auth = { login: 'admin', password: 'qwerty' }; // change this
+    // parse login and password from headers
+    const b64auth = (req.headers.authorization || '').split(' ')[1] || '';
+    const [login, password] = Buffer.from(b64auth, 'base64').toString().split(':');
+    // Verify login and password are set and correct
+    if (login && password && login === auth.login && password === auth.password) {
+        // Access granted...
+        return next();
     }
-    else {
-        res.sendStatus(401);
-    }
-};
+    // Access denied...
+    res.set('WWW-Authenticate', 'Basic realm="401"'); // change this
+    res.status(401).send('Authentication required.'); // custom message
+    // -----------------------------------------------------------------------
+});
 exports.postsRouter = (0, express_1.Router)();
 exports.postsRouter.get('/', (req, res) => {
     const getAllPosts = posts_repository_1.postsRepository.returnOfAllPosts;
--- src/routers/blogs-router.ts
+++ src/routers/blogs-router.ts
@@ -2,14 +2,30 @@ import {NextFunction, Request, Response, Router} from "express";
 import {blogsRepository} from "../repositories/blogs-repository";
 import {checkErrors} from "../errors/check-errors";
 
-const authGuardMiddleware = (req: Request, res: Response, next: NextFunction) => {
-    if (req.query.login === "admin" && req.query.password === "qwerty") {
-        next();
-    } else {
-        res.sendStatus(401)
+const authGuardMiddleware = ((req: Request, res: Response, next: NextFunction) => {
+
+    // -----------------------------------------------------------------------
+    // authentication middleware
+
+    const auth = {login: 'admin', password: 'qwerty'} // change this
+
+    // parse login and password from headers
+    const b64auth = (req.headers.authorization || '').split(' ')[1] || ''
+    const [login, password] = Buffer.from(b64auth, 'base64').toString().split(':')
+
+    // Verify login and password are set and correct
+    if (login && password && login === auth.login && password === auth.password) {
+        // Access granted...
+        return next()
     }
 
-}
+    // Access denied...
+    res.set('WWW-Authenticate', 'Basic realm="401"') // change this
+    res.status(401).send('Authentication required.') // custom message
+
+    // -----------------------------------------------------------------------
+
+})
 export const blogsRouter = Router ()
 
 
